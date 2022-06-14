7a5048d797b8debf3d31990084ef46fb98657667
김민성 <minsoftk@gmail.com>
2022-06-14 12:21:01 +0900

    컴포넌트화 할때 slot에서 상위 컴포넌트로 데이터를 전달하는 법

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,35 +1,41 @@
 <template>
-	<div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
-		<div
-			style="cursor: pointer"
-			@click="moveToPage(todo.id)"
-			class="card-body p-2 d-flex align-items-center"
-		>
-			<div class="form-check flex-grow-1">
-				<!-- 자식에서 부모 데이터를 변경시켜서는 안된다. v-model을 쓰게 되면 props 데이터를 변경시키게 된다. -->
-				<input
-					class="form-check-input"
-					type="checkbox"
-					:value="todo.completed"
-					@click.stop="toggleTodo(index, $event)"
-				/>
-				<label
-					:style="todo.completed ? todoStyle : ''"
-					:class="{
-						todo: todo.completed,
-					}"
-					class="form-check-label"
-				>
-					{{ todo.subject }}
-				</label>
+	<List :items="todos">
+		<template v-slot:default="{ item, index }">
+			<div
+				style="cursor: pointer"
+				@click="moveToPage(item.id)"
+				class="card-body p-2 d-flex align-items-center"
+			>
+				<div class="form-check flex-grow-1">
+					<!-- 자식에서 부모 데이터를 변경시켜서는 안된다. v-model을 쓰게 되면 props 데이터를 변경시키게 된다. -->
+					<input
+						class="form-check-input"
+						type="checkbox"
+						:checked="item.completed"
+						@change="toggleTodo(index, $event)"
+						@click.stop
+					/>
+					<label
+						:style="item.completed ? todoStyle : ''"
+						:class="{
+							todo: item.completed,
+						}"
+						class="form-check-label"
+					>
+						{{ item.subject }}
+					</label>
+				</div>
+				<div>
+					<button
+						@click.stop="openModal(item.id)"
+						class="btn btn-danger btn-sm"
+					>
+						Delete
+					</button>
+				</div>
 			</div>
-			<div>
-				<button @click.stop="openModal(todo.id)" class="btn btn-danger btn-sm">
-					Delete
-				</button>
-			</div>
-		</div>
-	</div>
+		</template>
+	</List>
 	<teleport to="#modal">
 		<Modal v-if="showModal" @close-modal="closeModal" @delete-todo="deleteTodo">
 		</Modal>
@@ -39,12 +45,14 @@
 <script>
 import { useRouter } from 'vue-router';
 import Modal from '@/components/DeleteModal.vue';
+import List from '@/components/List.vue';
 import { ref } from 'vue';
 
 export default {
 	// props:['todos']
 	components: {
 		Modal,
+		List,
 	},
 	props: {
 		todos: {
